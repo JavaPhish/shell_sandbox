1ff17df217730cedd2726bbae8af9c72791adfdd
CARTER_CLEMENTS <831@holbertonschool.com>
2019-08-19 19:50:20 -0400

    A fucking header

--- sandbox.h
+++ sandbox.h
@@ -1,2 +1,8 @@
+#ifndef SANDBOX_H
+#define SANDBOX_H
+
 int _contains(char *str, char *keyword);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *str_concat(char *s1, char *s2);
+
+#endif
