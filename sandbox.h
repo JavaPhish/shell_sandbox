881ecc3319a5b41e61ad1cdfb334245b93097b2e
CARTER_CLEMENTS <831@holbertonschool.com>
2019-08-21 17:46:08 -0400

    Updated header

--- sandbox.h
+++ sandbox.h
@@ -6,5 +6,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *str_concat(char *s1, char *s2);
 char **cmd_parser(char *cmd);
 char *_strdup(char *str);
+char *find_path(char *envp[]);
 
 #endif
