d942bb4d2da99d1839fcab06b936971b62969656
CARTER_CLEMENTS <831@holbertonschool.com>
2019-08-21 16:42:32 -0400

    Updated header

--- sandbox.h
+++ sandbox.h
@@ -4,5 +4,7 @@
 int _contains(char *str, char *keyword);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *str_concat(char *s1, char *s2);
+char **cmd_parser(char *cmd);
+char *_strdup(char *str);
 
 #endif
