22f09f3fb3cdb9bbc8346b8a589f96bf2b60c1de
CARTER_CLEMENTS <831@holbertonschool.com>
2019-08-25 14:26:50 -0400

    Gets a line and pushes it to execution helper

--- get_line.c
+++ get_line.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[], char *envp[])
 {
 	char *buffer;
 	size_t buffer_size = 0;
-	size_t chars_printed;
+	int chars_printed;
 
 	(void)argc;
 	(void)argv;
 
-	while (chars_printed != (size_t)EOF)
+	while (chars_printed != EOF)
 	{
 		write(1, "$ ", sizeof(char) * 2);
 		chars_printed = getline(&buffer, &buffer_size, stdin);
