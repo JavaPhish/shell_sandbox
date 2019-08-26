f82bcc569ead1c8ca76370902a0be0d2bda169a2
CARTER_CLEMENTS <831@holbertonschool.com>
2019-08-26 15:34:19 -0400

    Fixed several edge cases and added filepath functionality

--- sandbox_shell_v2/execution_handler.c
+++ sandbox_shell_v2/execution_handler.c
@@ -19,19 +19,25 @@ int execution_handler(char *cmd, char **env)
         char *path_with_cmd;
         char **arr_of_args;
         char *path_tokenizer;
+	int path_attempts;
 
         /*Calls a function which seperates the commands
         individual components into slots in an array.
         This is done for use in execve(filepath, command arguments)*/
         arr_of_args = cmd_parser(cmd);
 
+
         /*Attempts to execve with every file within the path
         once it find the correct one (Correct one being the one containing
         the requested command) it runs the command*/
         path_tokenizer = strtok(bin_path, ":");
+	path_attempts = 0;
+
+	path_with_cmd = malloc(sizeof(cmd));
+	_strcpy(path_with_cmd, cmd);
+
         while (path_tokenizer != NULL)
         {
-          path_with_cmd = str_concat(path_tokenizer, str_concat("/", cmd));
 
                 if (access(path_with_cmd, F_OK) == 0)
                 {
@@ -39,8 +45,12 @@ int execution_handler(char *cmd, char **env)
                         return (1);
                 }
 
-                path_tokenizer = strtok(NULL, ":");
-        }
+		path_with_cmd = str_concat(path_tokenizer, str_concat("/", arr_of_args[0]));
+
+		if (path_attempts != 0)
+                	path_tokenizer = strtok(NULL, ":");
+       		path_attempts++;
+	 }
 
         return 0;
 }
